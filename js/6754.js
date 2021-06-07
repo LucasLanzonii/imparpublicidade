0c3cb5f23dc79973111c9e38731fe3c1d62342cc
Rafael Giro <rafael.giro@aluno.ifsp.edu.br>
2021-06-07 18:01:41 -0300

    v2

---
+++ js/6754.js
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>Index of /</title>
+ </head>
+ <body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+<link rel="shortcut icon" href="/images-ampps/ampps.ico" />
+<title>[AMPPS] - Web Local</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+body {margin:0px;padding:0px;background:#ffffff;font-family:verdana, arial, helvetica, sans-serif;font-size:12px;}
+pre {font-size:140%;}
+a {color: #333333; text-decoration:none;}
+a:hover {color:#465584;text-decoration:none;}
+table {font-size: 100%;margin:0px auto 0px auto;padding:0px;}
+td {margin:0px;padding:0px 10px 0px 10px;}
+.footer {
+border-bottom:1px solid #cae0e5;
+border-top:1px solid #cae0e5;
+padding:8px;
+background-color: #E9EFFE;
+font-size:12px;
+}
+</style>
+</head>
+
+<body>
+
+<table width="780"s cellpadding="0" cellspacing="0" align="center" border="0">
+<tr>
+<td style="padding:0px;">
+<a href="http://www.ampps.com" target="_blank"><img src="/images-ampps/header_bkg.jpg" alt="AMPPS" border="0" style="float:left; " /></a>
+</td>
+</tr>
+</table>
+<br />
+<br />
+<center>
+<div style="width:780px;margin:0px auto 0px auto;padding:20px 0px 0px 0px;">  <table>
+   <tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
+   <tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="cgi-bin/">cgi-bin/</a>               </td><td align="right">2021-05-20 19:36  </td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="error/">error/</a>                 </td><td align="right">2021-05-20 19:35  </td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/image2.gif" alt="[IMG]"></td><td><a href="favicon.ico">favicon.ico</a>            </td><td align="right">2018-03-27 18:00  </td><td align="right">1.1K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="wp/">wp/</a>                    </td><td align="right">2020-02-06 09:03  </td><td align="right">  - </td><td>&nbsp;</td></tr>
+   <tr><th colspan="5"><hr></th></tr>
+</table>
+</center><br /><br /><br /><br />
+
+<table width="760" cellpadding="0" cellspacing="0" align="center" border="0" class="footer">
+<tr>
+<td style="padding:0px;">
+<a href="http://www.ampps.com" target="_blank">Powered by AMPPS</a> and <a href="http://www.softaculous.com" target="_blank">Softaculous</a>
+</td>
+</tr>
+</table>
+</div>
+
+</body>
+</html></body></html>
+
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
